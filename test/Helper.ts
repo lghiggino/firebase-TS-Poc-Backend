f27fbce607799d2ce53a4482d14ec75d9790d594
lghiggino <lghiggino@gmail.com>
2021-08-20 12:10:57 -0300

    Helper class and function

---
+++ test/Helper.ts
@@ -0,0 +1,44 @@
+import { fb, db } from "../src/firebase";
+
+export default class Helper {
+    // static async deleteQueryBatch(db, query, resolve) {
+    //     const snapshot = await query.get();
+
+    //     const batchSize = snapshot.size;
+    //     if (batchSize === 0) {
+    //         // When there are no documents left, we are done
+    //         resolve();
+    //         return;
+    //     }
+
+    //     // Delete documents in a batch
+    //     const batch = db.batch();
+    //     snapshot.docs.forEach((doc) => {
+    //         batch.delete(doc.ref);
+    //     });
+    //     await batch.commit();
+
+    //     // Recurse on the next process tick, to avoid
+    //     // exploding the stack.
+    //     process.nextTick(() => {
+    //         this.deleteQueryBatch(db, query, resolve);
+    //     });
+    // }
+
+    // static async deleteCollection(db, collectionPath, batchSize) {
+    //     const collectionRef = db.collection(collectionPath);
+    //     const query = collectionRef.orderBy('__name__').limit(batchSize);
+
+    //     return new Promise((resolve, reject) => {
+    //         this.deleteQueryBatch(db, query, resolve).catch(reject);
+    //     });
+    // }
+
+    static async deleteOneByOne(collectionName){
+        const snapshot = await db.collection(collectionName).get()
+        snapshot.forEach((doc) => {
+            doc.ref.delete()
+        })
+    }
+
+}
--- test/firebase.test.ts
+++ test/firebase.test.ts
@@ -1,6 +1,13 @@
 import { fb, db } from "../src/firebase";
 
 describe("Initial firebase tests", () => {
+    beforeEach(async () => {
+        const snapshot = await db.collection("users").get()
+        snapshot.forEach((doc) => {
+            doc.ref.delete()
+        })
+      });
+
     it.skip("should initialize firebase", async () => {
         const connection = await fb.firestore()
         console.log(connection)
@@ -16,13 +23,23 @@ describe("Initial firebase tests", () => {
         console.log(result)
     });
 
-    it("should read all documents from a collectionn", async () => {
+    it("should read all documents from a collection", async () => {
         const snapshot = await db.collection("users").get()
         snapshot.forEach((doc) => {
             console.log(doc.id, "->", doc.data())
         })
-        expect(snapshot).toBeDefined()
-        
+        // expect(snapshot).toBeDefined()
+    })
+
+    it.skip("should create a new collection and new document", async () => {
+        const data = {
+            name: 'Los Angeles',
+            state: 'CA',
+            country: 'USA'
+        };
+
+        // Add a new document in collection "cities" with ID 'LA'
+        const res = await db.collection('cities').doc('LA').set(data);
     })
 
 });
