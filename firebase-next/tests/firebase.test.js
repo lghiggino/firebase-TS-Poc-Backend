9f6c902895c6059b684396d65a8ccb9b772d11d5
lghiggino <lghiggino@gmail.com>
2021-09-16 22:13:37 -0300

    firebase observer test

--- firebase-next/tests/firebase.test.js
+++ firebase-next/tests/firebase.test.js
@@ -33,13 +33,8 @@ beforeEach(async () => {
     });
 })
 
-describe("GET", () => {
-    it("get ONE document BY REFERENCE", async () => {
-        const cityRef = db.collection('cities').doc('TOK');
-        const doc = await cityRef.get();
-        expect(doc.data().name).toBe("Tokyo")
-    })
-
+describe.skip("GET", () => {
+    //https://firebase.google.com/docs/firestore/query-data/get-data
     it("should read ALL documents in the collection", async () => {
         const citiesRef = db.collection('cities');
         const snapshot = await citiesRef.get();
@@ -49,6 +44,12 @@ describe("GET", () => {
         expect(snapArray).toContain("Washington, D.C.")
     });
 
+    it("should get ONE document BY REFERENCE", async () => {
+        const cityRef = db.collection('cities').doc('TOK');
+        const doc = await cityRef.get();
+        expect(doc.data().name).toBe("Tokyo")
+    })
+
     it("should get multiple documents from a collection based on a query", async () => {
         const citiesRef = db.collection("cities")
         const snapshot = await citiesRef.where("capital", "==", true).get()
@@ -56,7 +57,7 @@ describe("GET", () => {
         snapshot.forEach(doc => {
             snapArray.push(doc.data())
         })
-        console.log(snapArray)
+        // console.log(snapArray)
         expect(snapArray).toHaveLength(3)
         expect(snapArray).not.toContain(
             {
@@ -65,10 +66,37 @@ describe("GET", () => {
             }
         )
     })
-    //https://firebase.google.com/docs/firestore/query-data/get-data
-
-    it.skip("get ONE document BY QUERY", () => {
-
-    })
 })
 
+describe("GET Realtime Updates", () => {
+    it("should listen to realtime updates to a document", async () => {
+        const doc = db.collection("cities").doc("SF")
+        await doc.update({
+            newAtrribute: new Date().toLocaleString()
+        })
+
+        const promise = new Promise((resolve, reject) => {
+            const observer = doc.onSnapshot(docSnapshot => {
+                console.log(`Received doc snapshot: ${docSnapshot}`)
+                resolve(docSnapshot)
+                //desconecta o listener
+                observer()
+            }, error => {
+                console.log(`Encountered error: ${error}`)
+                reject(error)
+                observer()
+            })
+        })
+
+        await doc.update({
+            newAtrribute2: new Date().toLocaleString()
+        })
+
+        const modifiedDoc = await promise
+        
+        console.log(modifiedDoc.data())
+
+        expect(modifiedDoc).toBeDefined()
+        expect(modifiedDoc.data().newAtrribute).toBeDefined()
+    })
+})
